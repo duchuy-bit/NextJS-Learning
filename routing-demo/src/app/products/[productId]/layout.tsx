f0ea98e76a4815d068947e2410eba058a78df159
duchuy-bit <quysunho123@gmail.com>
2024-06-09 15:58:19 +0700

    layout

---
+++ routing-demo/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,8 @@
+export default function ProductDetailLayout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            {children}
+            <h2> Feature Layout</h2>
+        </section>
+    );
+}
